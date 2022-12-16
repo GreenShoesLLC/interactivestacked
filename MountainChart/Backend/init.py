bb402ab5304e5121785243caa138bcd2b3c9ed44
edenhazard701 <techrub710@gmail.com>
2022-12-16 16:55:39 -0500

    backend cors error debug

--- MountainChart/Backend/init.py
+++ MountainChart/Backend/init.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from controller import api
 from config import db_URI, secret
@@ -8,7 +9,8 @@ def create_app(test_config=None):
 
     # creates an application that is named after the name of the file
     app = Flask(__name__)
-
+    CORS(app)
+    
     app.config["SECRET_KEY"] = secret
     app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
 
