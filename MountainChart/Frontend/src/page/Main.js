7a3c27ac9bbeab5aff4f21985c32dffbb8dbc273
edenhazard701 <techrub710@gmail.com>
2023-01-20 10:33:30 -0500

    restore chart-1

--- MountainChart/Frontend/src/page/Main.js
+++ MountainChart/Frontend/src/page/Main.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import moment from 'moment';
 
 import MountainChart from 'component/chart/MountainChart';
-import { getDataByWorkspaceId } from 'store/actions/queries/workspace';
 
+import { GET_DATA } from 'store/actions/queries/workspace';
 import { UPDATE_CAPACITY_BY_DRAG } from 'store/actions/mutations/resource';
 import { UPDATE_PRORES_BY_RESIZE } from 'store/actions/mutations/projectReource';
 import { UPDATE_PROJECT_BY_DRAG } from 'store/actions/mutations/project';
@@ -14,21 +15,39 @@ const Main = () => {
     resource: 'Developers',
     portfolio: 'Portfolio1'
   });
-  
-  const data = getDataByWorkspaceId('V29ya3NwYWNlOjE=');
-
-  const [updateRes] = useMutation(UPDATE_CAPACITY_BY_DRAG);
-  const [updateDemand] = useMutation(UPDATE_PRORES_BY_RESIZE);
-  const [updateProject] = useMutation(UPDATE_PROJECT_BY_DRAG);
 
   const chartRef = useRef(null), portRef = useRef(null), resRef = useRef(null);
+
   const resourceType = {
     'Developers':'Developers',
     'QA':'QA',
     'Business Analyst': 'Business Analyst'
   };
-
   const portfolioType = ['Portfolio1', 'Portfolio2'];
+  
+  const { data, refetch } = useQuery(GET_DATA, {
+    notifyOnNetworkStatusChange: true,
+    variables: {workspaceId: 'V29ya3NwYWNlOjE='}
+  });
+
+  const [updateRes] = useMutation(UPDATE_CAPACITY_BY_DRAG, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: () => {
+      refetch();
+    }
+  });
+  const [updateDemand] = useMutation(UPDATE_PRORES_BY_RESIZE, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: () => {
+      refetch();
+    }
+  });
+  const [updateProject] = useMutation(UPDATE_PROJECT_BY_DRAG, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: () => {
+      refetch();
+    }
+  });
 
   const handleMethod = () => {
     setFilter({
@@ -37,21 +56,65 @@ const Main = () => {
     });
   }
 
-  const onSourceChange = (change) => {
+  const convertData = (data) => {
+    if(!data) return;
+    const { workspace } = data;
+    let cap = {};
+    let project = {};
+    let portfolio = {};
+    workspace.resources.edges.map((row) => {
+      let { Id, Name, BaselineCapacity, StartAt, pro } = row.node;
+      cap[Name] = {
+        Id,
+        BaselineCapacity: JSON.parse(BaselineCapacity),
+        startAt: moment(StartAt).format('YYYY.MM')
+      }
+
+      portfolio[Name] = [];
+      pro.edges.map((item) => {
+        let { BaselineDemand, project, ProjectId, ResourceId } = item.node;
+        portfolio[Name].push({
+          name: project.Name,
+          data: JSON.parse(BaselineDemand),
+          pId: ProjectId,
+          rId: ResourceId
+        })
+      })
+    });
+
+    workspace.projects.edges.map((row) => {
+      let { Id, Color, StrokeColor, Name, BaselineStartDate, BaselinePriority } = row.node;
+      project[Name] = {
+        Id,
+        start: moment(BaselineStartDate).format('YYYY.MM'),
+        priority: BaselinePriority,
+        color: Color,
+        strokecolor: StrokeColor,
+      }
+    });
+
+    return {
+      cap, 
+      project, 
+      portfolio
+    };
+  };
+
+  const onSourceChange = async (change) => {
     const {state, newData} = change;
     switch(state){
       case 'resize': {
         let { pId, rId, data } = newData;
-        updateDemand({variables: {pId, rId, Demand: data}});
+        await updateDemand({variables: {pId, rId, Demand: data}});
         break;
       }
       case 'drag': {
-        updateProject({variables: {ProjectList: newData}});
+        await updateProject({variables: {ProjectList: newData}});
         break;
       }
       case 'capacity': {
         let { data, index } = newData;
-        updateRes({variables: {Id: index, capacity: `[${data.toString()}]`}});
+        await updateRes({variables: {Id: index, capacity: `[${data.toString()}]`}});
         break;
       }
       default:
@@ -97,7 +160,7 @@ const Main = () => {
       <Selector />
       <MountainChart  
         title = {'MountainChart-1'}
-        chartdata = {data}
+        chartdata = {convertData(data)}
         AxisXMin = {2022}
         AxisXMax = {2026}
         AxisXLabel = {'Time'}
@@ -107,16 +170,16 @@ const Main = () => {
         stateChange = { onSourceChange }
         filter = { filter }
         />
-      <MountainChart  
+      <MountainChart
         title = {'MountainChart-2'}
-        chartdata = {data}
+        chartdata = {convertData(data)}
         AxisXMin = {2023}
         AxisXMax = {2026}
         AxisXLabel = {'year/month'}
         AxisYMax = {40}
         AxisYInterval = {5}
-        stateChange = { onSourceChange }
-        filter = { filter }
+        stateChange = {onSourceChange}
+        filter = {filter}
         />
     </>
   )
--- MountainChart/Frontend/src/store/actions/queries/workspace.js
+++ MountainChart/Frontend/src/store/actions/queries/workspace.js
@@ -1,37 +1,34 @@
-import { useQuery, gql } from '@apollo/client';
-import moment from 'moment';
+import { gql } from '@apollo/client';
 
-export const getDataByWorkspaceId = (workspaceId) => {
-  const GET_DATA = gql`query {
-    workspace(id: "${workspaceId}"){
-      projects{
-        edges{
-          node{
-            Id,
-            Name,
-            BaselineStartDate,
-            BaselinePriority,
-            Color,
-            StrokeColor
-          }
+export const GET_DATA = gql`query ($workspaceId: ID!){
+  workspace(id: $workspaceId){
+    projects{
+      edges{
+        node{
+          Id,
+          Name,
+          BaselineStartDate,
+          BaselinePriority,
+          Color,
+          StrokeColor
         }
-      },
-      resources{
-        edges{
-          node{
-            Id,
-            Name,
-            BaselineCapacity,
-            StartAt
-            pro{
-              edges{
-                node{
-                  ProjectId,
-                  ResourceId,
-                  BaselineDemand
-                  project{
-                    Name
-                  }
+      }
+    },
+    resources{
+      edges{
+        node{
+          Id,
+          Name,
+          BaselineCapacity,
+          StartAt
+          pro{
+            edges{
+              node{
+                ProjectId,
+                ResourceId,
+                BaselineDemand
+                project{
+                  Name
                 }
               }
             }
@@ -39,55 +36,5 @@ export const getDataByWorkspaceId = (workspaceId) => {
         }
       }
     }
-  }`;
-
-  const { loading, error, data, refetch } = useQuery(GET_DATA, {
-    fetchPolicy: 'no-cache',
-  });
-  refetch(GET_DATA);
-  
-  if(error) return error;
-
-  if(data) {
-    const { workspace } = data;
-    let cap = {};
-    let project = {};
-    let portfolio = {};
-    workspace.resources.edges.map((row) => {
-      let { Id, Name, BaselineCapacity, StartAt, pro } = row.node;
-      cap[Name] = {
-        Id,
-        BaselineCapacity: JSON.parse(BaselineCapacity),
-        startAt: moment(StartAt).format('YYYY.MM')
-      }
-
-      portfolio[Name] = [];
-      pro.edges.map((item) => {
-        let { BaselineDemand, project, ProjectId, ResourceId } = item.node;
-        portfolio[Name].push({
-          name: project.Name,
-          data: JSON.parse(BaselineDemand),
-          pId: ProjectId,
-          rId: ResourceId
-        })
-      })
-    });
-
-    workspace.projects.edges.map((row) => {
-      let { Id, Color, StrokeColor, Name, BaselineStartDate, BaselinePriority } = row.node;
-      project[Name] = {
-        Id,
-        start: moment(BaselineStartDate).format('YYYY.MM'),
-        priority: BaselinePriority,
-        color: Color,
-        strokecolor: StrokeColor,
-      }
-    });
-
-    return {
-      cap, 
-      project, 
-      portfolio
-    };
   }
-}
+}`;
